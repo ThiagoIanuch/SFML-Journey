3a5d024b5987e65e12c84a04a80953daf77cbaf5
ThiagoIanuch <thiagoianuch@hotmail.com>
2024-10-18 11:19:06 -0300

    Add foreach to draw circles

--- 02 - Pseudo Random Circles/main.cpp
+++ 02 - Pseudo Random Circles/main.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main() {
+int main() 
+{
 	sf::RenderWindow window(sf::VideoMode(800, 600), "02 - Pseudo Random Circles");
 
 	sf::CircleShape circleShape;
@@ -10,17 +11,21 @@ int main() {
 
 	srand(time(NULL)); // Semente para gerar os circulos de forma pseudo aleatória
 
-	while (window.isOpen()) {
+	while (window.isOpen()) 
+	{
 		// Fechar a janela
 		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
+		while (window.pollEvent(event)) 
+		{
+			if (event.type == sf::Event::Closed) 
+			{
 				window.close();
 			}
 		}
 				
 		// Gerar os círculos
-		if(clock.getElapsedTime().asMicroseconds() > 5) {
+		if(clock.getElapsedTime().asMicroseconds() > 5) 
+		{
 			circles.push_back(circleShape);
 
 			circles.back().setRadius(rand() % 25 + 15);
@@ -36,8 +41,9 @@ int main() {
 		window.clear();
 
 		// Desenhar os círculos na tela
-		for (int i = 0; i < circles.size(); i++) {
-			window.draw(circles[i]);
+		for (sf::CircleShape c : circles) 
+		{
+			window.draw(c);
 		}
 
 		window.display();
