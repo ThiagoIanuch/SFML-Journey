120411c5fc320a536a2daac6ca6bd1bcd5309c6f
ThiagoIanuch <thiagoianuch@hotmail.com>
2024-10-19 11:24:22 -0300

    Fix screen collision

--- 03 - Pseudo random circle collisions/main.cpp
+++ 03 - Pseudo random circle collisions/main.cpp
@@ -7,7 +7,7 @@ const int WINDOW_HEIGHT = 600;
 const int FRAMERATE_LIMIT = 60;
 
 // Circles properties
-const int NUMBER_OF_CIRCLES = 10;
+const int NUMBER_OF_CIRCLES = 30;
 
 int main()
 {
@@ -16,7 +16,7 @@ int main()
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "03 - Pseudo random circle collisions");
 
     sf::Vector2f movement[50];
-    
+
     window.setFramerateLimit(FRAMERATE_LIMIT);
 
     srand(time(NULL));
@@ -29,10 +29,10 @@ int main()
         circles[i].setFillColor(sf::Color::Blue);
     }
 
-    while (window.isOpen()) 
+    while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event)) 
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) {
                 window.close();
@@ -45,11 +45,12 @@ int main()
         {
             circles[i].move(movement[i]);
 
-            if (circles[i].getPosition().x > 800 || circles[i].getPosition().x < 0 || circles[i].getPosition().y > 600 || circles[i].getPosition().y < 0)
+            if (circles[i].getPosition().x > 800 || circles[i].getPosition().x < 0)
             {
-                movement[i] = -movement[i];
-
-                circles[i].setRotation(circles[i].getRotation() + 10);
+                movement[i].x = -movement[i].x;
+            }
+            else if (circles[i].getPosition().y > 600 || circles[i].getPosition().y < 0) {
+                movement[i].y = -movement[i].y;
             }
         }
 
